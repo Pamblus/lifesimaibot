af227255a334c95707ab5130cba49e572854569d
Pamblus <79162626+Pamblus@users.noreply.github.com>
2025-10-03 21:38:31 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Конфигурация
+API_KEY = "sk-or-v1-d6378***************321da6b"
+TELEGRAM_TOKEN = "73*****65:AAFNEYY************7kY"
+PROMPT_FILE = "prompt.txt"
+USERS_DIR = "users"
+
+# Лимиты
+MAX_INVENTORY_ITEMS = 999
